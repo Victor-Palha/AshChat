17d2dac8ecfbb95aaf4380f16a080a49c6bcc1d6
strahd <jv.palha@gmail.com>
2024-11-12 17:46:22 -0300

    add: verification of device token to acess websocket server

--- backend/src/websocket/index.ts
+++ backend/src/websocket/index.ts
@@ -3,6 +3,7 @@ import { Events } from "./factory/events.factory";
 import { changeUserStatusFactory } from "../domain/factories/change-user-status.factory";
 import { verify } from "jsonwebtoken";
 import { env } from "../config/env";
+import { verifyUniqueTokenDeviceFactory } from "../domain/factories/verify-user-unique-token-device.factory";
 
 interface PayloadJWT{
     sub: string
@@ -23,6 +24,9 @@ export class IOServer {
             try {
                 const jwt_token = socket.handshake.auth.token as string
                 const userId = this.verifyAuthByToken(jwt_token)
+                const device_token = socket.handshake.auth.device_token as string
+                this.verifyUserDeviceToken({ user_id: userId, device_token })
+                
                 socket.join(`user_${userId}`)
                 changeUserStatus.execute({ userId, online: true})
 
@@ -41,7 +45,7 @@ export class IOServer {
         })
     }
 
-    public verifyAuthByToken(token: string): string {
+    private verifyAuthByToken(token: string): string {
         if (!token) {
             throw new Error("Authentication error: No token provided");
           }
@@ -54,6 +58,14 @@ export class IOServer {
           } catch (error) {
             throw new Error("Authentication error: Invalid token");
           }
+    }
+
+    private async verifyUserDeviceToken({ user_id, device_token }: { user_id: string, device_token: string }): Promise<void> {
+        const service = verifyUniqueTokenDeviceFactory()
+        const {isValid} = await service.execute({ user_id, deviceUniqueToken: device_token })
+        if(!isValid){
+            throw new Error("Authentication error: Invalid device token")
+        }
     }
     
 }
