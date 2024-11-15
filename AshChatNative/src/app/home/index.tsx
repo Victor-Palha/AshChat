2810cffeb966477b0afacb56e0bf9d3677144cc6
strahd <jv.palha@gmail.com>
2024-11-15 16:26:43 -0300

    add: home page layout

--- AshChatNative/src/app/home/index.tsx
+++ AshChatNative/src/app/home/index.tsx
@@ -1,29 +1,35 @@
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View, Image } from "react-native";
 
 export default function Home(){
     return (
-        <View className="flex-1 pt-[62px] items-center justify-center text-white px-10" >
+        <View className="flex-1 pt-[62px] px-10" >
             {/* Header */}
-            <View className="justify-end items-center py-5">
-                <TouchableOpacity className="bg-purple-700 rounded-full">
+            <View className="items-end py-5">
+                <TouchableOpacity className="bg-purple-700 rounded-full p-1">
                     <MaterialIcons name="add" size={24} color="white" />
                 </TouchableOpacity>
             </View>
 
             <Text className="font-bold text-white text-3xl">My Chats</Text>
 
-            <View className="flex-row items-center justify-around">
+            <View className="flex-row items-center justify-between mt-5">
                 <TouchableOpacity className="flex-row items-center gap-2">
                     <Ionicons name="chatbubble-outline" size={24} color="white" />
-                    <Text>All</Text>
+                    <Text className="text-white font-bold">All</Text>
                 </TouchableOpacity>
                 <TouchableOpacity className="flex-row items-center gap-2">
                     <Ionicons name="chatbubble-ellipses-outline" size={24} color="white" />
-                    <Text>Unread</Text>
+                    <Text className="text-white font-bold">Unread</Text>
                 </TouchableOpacity>
             </View>
-            
+            <View className="items-center justify-center pt-[30%]">
+                <Image
+                    className="rounded-full w-100 h-100"
+                    source={require('../../assets/nowhere.png')}
+                />
+                <Text className="text-white font-semibold italic text-md mt-[-40] text-center">Hmm... You seen to be lost! Try to connect to someone!</Text>
+            </View>
         </View>
     )
 }
