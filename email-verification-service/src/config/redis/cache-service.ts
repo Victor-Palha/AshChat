cc3a99c458aa20ce320b496991a13d7400775223
strahd <jv.palha@gmail.com>
2024-11-21 16:11:18 -0300

    chore: change name service

--- email-verification-service/src/config/redis/cache-service.ts
+++
@@ -1,5 +0,0 @@
-export abstract class CacheService {
-    abstract setItem(key: string, seconds: number, value: string): Promise<void>;
-    abstract get(key: string): Promise<string | null>;
-    abstract delete(key: string): Promise<void>;
-}
