3c79ccd5380b27a27012671e31e558463af0d2ae
strahd <jv.palha@gmail.com>
2024-11-27 16:43:25 -0300

    add: docker config to translate service

--- translate_service/rabbitmq/connection.py
+++ translate_service/rabbitmq/connection.py
@@ -1,8 +1,18 @@
 import pika
+import os
+from dotenv import load_dotenv, dotenv_values 
+load_dotenv()
 
+py_rabbitmq_host = os.getenv("PY_RABBITMQ_HOST")
+py_rabbitmq_username = os.getenv("PY_RABBITMQ_USER")
+py_rabbitmq_password = os.getenv("PY_RABBITMQ_PASSWORD")
+
+print(py_rabbitmq_host)
+print(py_rabbitmq_username)
+print(py_rabbitmq_password)
 
 class RabbitMQConnection:
-    def __init__(self, username="root", password="randompassword", host="localhost", port=5672):
+    def __init__(self, username=py_rabbitmq_username, password=py_rabbitmq_password, host=py_rabbitmq_host, port=5672):
         self.credentials = pika.PlainCredentials(username, password)
         self.host = host
         self.port = port
