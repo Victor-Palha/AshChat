9b9a45dce1758ef63abdbc2d659ff4b9a4af2ccc
strahd <jv.palha@gmail.com>
2025-02-12 23:28:08 -0300

    styles: change where user photo appers

--- ashchat-desktop/src/renderer/src/screens/Home/SideBar/index.tsx
+++ ashchat-desktop/src/renderer/src/screens/Home/SideBar/index.tsx
@@ -5,17 +5,19 @@ type SideBarProps = {
 }
 export function SideBar(data: SideBarProps){
     return (
-        <aside className="flex flex-col p-5 gap-5 bg-gray-900">
-            <img 
-                src="http://localhost:3006/files/04a76336-3571-4680-a55a-ad6ee5dfff62.jpg" 
-                className="cursor-pointer rounded-full w-[50px] h-[50px] hover:scale-105 transition"
-            />
+        <aside className="flex flex-col p-5 gap-5 bg-gray-900 justify-between">
             <button
                 className="flex bg-purple-700 rounded-full w-[50px] h-[50px] items-center justify-center"
                 onClick={data.handleOpenModal}
             >
                 <Plus size={24} color="white" />
             </button>
+            <div>
+                <img 
+                    src="http://localhost:3006/files/04a76336-3571-4680-a55a-ad6ee5dfff62.jpg" 
+                    className="cursor-pointer rounded-full w-[50px] h-[50px] hover:scale-105 transition"
+                />
+            </div>
         </aside>
     )
 }
